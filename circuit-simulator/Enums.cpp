b559dbbd8951a3aae5e56d32719898b588adbdb6
Josiah Lansford <redrobbin99@gmail.com>
2019-11-28 17:36:57 -0500

    finished GateType strToGateType()

--- circuit-simulator/Enums.cpp
+++ circuit-simulator/Enums.cpp
@@ -1,6 +1,16 @@
+#pragma once
 #include "Enums.h"
 
+/*
+* Convert string input to GateType.
+* Assumes valid input of one of the enums, returns NOT by default.
+*/
 GateType strToGateType(std::string str) {
-  // TODO: switch statement that returns enum
-  return GateType::AND;
+  if (str == "AND") { return GateType::AND; }
+  else if (str == "OR") { return GateType::OR; }
+  else if (str == "XOR") { return GateType::XOR; }
+  else if (str == "NAND") { return GateType::NAND; }
+  else if (str == "NOR") { return GateType::NOR; }
+  else if (str == "XNOR") { return GateType::XNOR; }
+  else { return GateType::NOT; }
 }
