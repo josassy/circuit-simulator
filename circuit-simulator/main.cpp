da463e0c1949413004ebbbf82088575e33f78418
Ben Harless <ben4jc@juno.com>
2019-12-03 01:10:56 -0500

    fstream input stuff

--- circuit-simulator/main.cpp
+++ circuit-simulator/main.cpp
@@ -2,10 +2,96 @@
 #include <queue>
 #include "Event.h"
 #include "Enums.h"
+#include <fstream>
+#include <cstring>
 
 using namespace Enums;
 
 int main() {
+
+  std::ifstream inFS;
+
+  std::string fileName;
+  fileName = "circuit0.txt";
+  inFS.open(fileName);
+  char* s;
+
+  
+  inFS.getline(s, 256);
+
+  char* temp1;
+  std::vector<char*> temp2;
+  temp1 = strtok(s, " ");
+  temp2.push_back(temp1);
+
+  while (temp1 != NULL) {
+    temp1 = strtok(s, " ");
+    temp2.push_back(temp1);
+  }
+
+  if (temp2[0] == "CIRCUIT") {
+    char* name = temp2[1];
+
+    while (!inFS.eof()) {
+
+      inFS.getline(s, 256);
+
+      char* temp1;
+      std::vector<char*> temp2;
+      temp1 = strtok(s, " ");
+      temp2.push_back(temp1);
+
+      while (temp1 != NULL) {
+        temp1 = strtok(s, " ");
+        temp2.push_back(temp1);
+      }
+
+      if (temp2[0] == "INPUT") {
+        char* wireName = temp2[1];
+        char* wireNum = temp2[2];
+        //to do, create wire with above info
+      }
+      else if (temp2[0] == "OUTPUT") {
+        char* wireName = temp2[1];
+        char* wireNum = temp2[2];
+        //to do, create wire with above info
+      }
+      else if (temp2[0] == "NOT") {
+        char* delay = temp2[1];
+        char* input1 = temp2[2];
+        char* output = temp2[3];
+        //to do, create gate containing above info
+      }
+      else {
+        char* delay = temp2[1];
+        char* input1 = temp2[2];
+        char* input2 = temp2[3];
+        char* output = temp2[4];
+        //to do, create gate containing above info
+      }
+
+    }
+  }
+  else if (temp2[0] == "VECTOR") {
+    while (!inFS.eof()) {
+
+      inFS.getline(s, 256);
+
+      char* temp1;
+      std::vector<char*> temp2;
+      temp1 = strtok(s, " ");
+      temp2.push_back(temp1);
+
+      while (temp1 != NULL) {
+        temp1 = strtok(s, " ");
+        temp2.push_back(temp1);
+      }
+
+    }
+  }
+    
+  
+
   std::cout << "Hello World!" << std::endl;
   
   std::priority_queue<Event> queue;
