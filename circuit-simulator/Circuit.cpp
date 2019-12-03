5b27da76373533dd6801d1bf8fb845df854dd3ad
Josiah Lansford <redrobbin99@gmail.com>
2019-12-02 23:45:11 -0500

    wrote processEvents functions

--- circuit-simulator/Circuit.cpp
+++ circuit-simulator/Circuit.cpp
@@ -1,2 +1,25 @@
 #include "Circuit.h"
 
+void Circuit::scheduleEvent(Event e) {
+  eventQueue.push(e);
+}
+
+void Circuit::processEvents() {
+  while (eventQueue.size() > 0 || !processDone) {
+    handleEvent(eventQueue.top());
+  }
+}
+
+void Circuit::handleEvent(Event e) {
+  // Print event details to debug
+  e.print();
+
+  // If a scheduled events time is more than 60 ms, stop processing
+  if (e.getTime > 60) {
+    processDone = true;
+  }
+
+  // Get pointer to target wire, and peform changes
+  Wire* target = wireArray.at(e.getWireNum());
+  target->setState(e.getWireValue());
+}
--- circuit-simulator/Circuit.h
+++ circuit-simulator/Circuit.h
@@ -5,8 +5,14 @@
 #include "Event.h"
 
 class Circuit {
+public:
+  void scheduleEvent(Event e);
+  void processEvents();
 
 private:
+  void handleEvent(Event e);
+  // Hold the number of milliseconds we have processed
+  bool processDone;
   std::vector<Wire*> wireArray;
   std::priority_queue<Event> eventQueue;
 };
